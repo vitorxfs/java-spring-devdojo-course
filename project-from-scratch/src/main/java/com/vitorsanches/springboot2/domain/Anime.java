b03f4410fee4735aa11c904c45bb6cfe0fd84605
Vitor <viisanches98@gmail.com>
2024-09-27 15:18:51 -0300

    create project from scratch

---
+++ project-from-scratch/src/main/java/com/vitorsanches/springboot2/domain/Anime.java
@@ -0,0 +1,16 @@
+package com.vitorsanches.springboot2.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
